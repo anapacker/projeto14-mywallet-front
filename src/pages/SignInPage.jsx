5b6f102c784783a4672650f34182a27e5eab278b
anapacker <anaamaria.packer@gmail.com>
2023-07-05 01:07:51 -0300

    Tela Sign-in (rota /)

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,61 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from "axios"
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isDisable, setIsDisable] = useState("")
+
+  const navigate = useNavigate()
+
+  function loginToAccont(e) {
+    e.preventDefault()
+    setIsDisable(true)
+
+    let sendDatasToAPi = {
+      email: email,
+      password: password
+    }
+
+    const promisse = axios.post(`htto://localhost:3000/usuario`, sendDatasToAPi)
+    promisse.then(res => {
+      navigate(`/home`)
+    })
+    promisse.catch(err => {
+      alert(err.response.data.message)
+    })
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={loginToAccont}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          disabled={isDisable}
+        />
+
+        <input
+          placeholder="Senha"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autocomplete="new-password"
+          required
+          disabled={isDisable}
+        />
+
+        <button type="submit" disabled={isDisable}>{!isDisable ? 'Entrar' : isDisable}</button>
       </form>
 
-      <Link>
+      <Link to="/cadastro">
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
